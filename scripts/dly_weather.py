948e8093596ec6db882da472b2e5bf61a7cd1117
erb3erb3erb3 <49728807+erb3erb3erb3@users.noreply.github.com>
2025-11-10 18:41:08 -0500

    Refactor NOAA weather ingestion script
    Updated NOAA API settings and added comments for clarity. Rewrote partition check function to indicate it needs to be implemented for S3.

--- scripts/dly_weather.py
+++ scripts/dly_weather.py
@@ -8,6 +8,7 @@ from pyspark.sql.functions import current_timestamp
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, DateType
 
 # Spark init
+# Config for S3 connection
 spark = (
     SparkSession.builder
     .appName("NOAA Daily Weather Ingestion")
@@ -17,23 +18,23 @@ spark = (
     .getOrCreate()
 )
 
-# NOAA API settings
-NOAA_TOKEN = os.getenv("NOAA_TOKEN")  # export this in your environment 
-STATION_ID = "GHCND:USW00014922"      # Minneapolis/St. Paul International
+# NOAA API settings: Station ID is for Daily Minneapolis/St.Paul
+NOAA_TOKEN = os.getenv("NOAA_TOKEN")  
+STATION_ID = "GHCND:USW00014922"      
 BASE_URL = "https://www.ncei.noaa.gov/cdo-web/api/v2/data"
 
 # Schema for the weather data
 weather_schema = StructType([
     StructField("station", StringType(), True),
     StructField("datatype", StringType(), True),
-    StructField("value", DoubleType(), True),   # always numeric
-    StructField("date", StringType(), True)      # YYYY-MM-DD
-    #StructField("ingestion_time", StringType(), True)  # will fill with current_timestamp later
+    StructField("value", DoubleType(), True),   
+    StructField("date", StringType(), True)      
 ])
 
 # Bronze output path
 bronze_path = "s3a://minneapolis-transit-lake/bronze/noaa_daily_weather"
 
+# Function to fetch Weather Data for a given date
 def fetch_noaa_daily(date):
     headers = {"token": NOAA_TOKEN}
     params = {
@@ -54,27 +55,32 @@ def fetch_noaa_daily(date):
         station=rec["station"],
         datatype=rec["datatype"],
         value=float(rec["value"]), # Ensure value is float to avoid merge issue in Spark
-        date=rec["date"][:10] # Extract YYYY-MM-DD
+        date=rec["date"][:10] # Extract YYYY-MM-DD from date timestamp
     ) for rec in results]
     return rows
 
-# Function to check if Partion exists 
-def partition_exists(date):
-    partition_folder = os.path.join(bronze_path, f"date={date}")
-    return os.path.exists(partition_folder)
+# Function to check if Partion exists for a given date
+
+# !!! NEED TO REWRITE TO CHECK PARTITION ON S3 !!!
+# def partition_exists(date):
+#     partition_folder = os.path.join(bronze_path, f"date={date}")
+#     return os.path.exists(partition_folder)
 
 
+# Function to write data to S3 bronze layer
 def write_to_bronze(rows):
     if not rows:
         print("No weather data to write.")
         return
     
-    # Check if partition for the date already exists, skip write if it does.
-    partition_date = rows[0].date if rows else None
-    if partition_exists(partition_date):
-        print(f"Partition for date {partition_date} already exists. Skipping write.")
-        return 
+    # Check if partition for the date already exists, skips write if it does.
+    # !!! NEED TO REWRITE partition_exists FUNC TO CHECKS S3 !!!
+    ### partition_date = rows[0].date if rows else None
+    ### if partition_exists(partition_date):
+    ###     print(f"Partition for date {partition_date} already exists. Skipping write.")
+    ###     return 
 
+    # Create spark Data Frame and write to S3 
     df = spark.createDataFrame(rows, schema=weather_schema)
     df = df.withColumn("date", df["date"].cast("date"))
     df = df.withColumn("ingestion_time", current_timestamp())
@@ -83,7 +89,8 @@ def write_to_bronze(rows):
 
 
 if __name__ == "__main__":
-    print(f"token = {NOAA_TOKEN}")
+
+    # Write daily going back as far as the set number of backfill days.
     backfill_days = 5
     for i in range(backfill_days):
         date = (datetime.now() - timedelta(days=i+1)).strftime("%Y-%m-%d")
